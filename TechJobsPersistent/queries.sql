7af006d2a3871c76c275656784b8900420fd85de
jszumowski <85529714+jszumowski@users.noreply.github.com>
2021-12-12 21:18:34 -0500

    finished the orm. updated missing asp keys in the addjob view and beautified code in the home controller

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,16 @@
 --Part 1
+--Columns - DataType
+--Id - INT
+--Name - VARCHAR
+--EmployerId - INT
 
 --Part 2
+SELECT Name
+From employers
+Where Location = "St. Louis";
 
 --Part 3
-
+SELECT Name, Description
+FROM skills
+Left JOIN jobskills on skills.Id = jobskills.SkillId where jobskills.JobId IS NOT NULL
+order by Name ASC;
